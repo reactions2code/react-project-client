cb8f0e632584b3fd6147fa744e75e66461f83326
skmidk <s.peraza545@gmail.com>
2020-08-24 14:02:42 -0400

    added book route, and page for book component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -54,6 +54,7 @@ class App extends Component {
           <AuthenticatedRoute user={user} path='/change-password' render={() => (
             <ChangePassword msgAlert={this.msgAlert} user={user} />
           )} />
+          <AuthenticatedRoute user={user} exact path='/books' 
         </main>
       </Fragment>
     )
