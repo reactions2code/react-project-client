8998ec8c83a9dad1a34ded2a4689497d465edb8d
skmidk <s.peraza545@gmail.com>
2020-08-25 14:32:58 -0400

    Post create added post create form added post route added post show route added post index added
    author: scott

---
+++ src/components/routes/Posts.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import apiUrl from '../../apiConfig'
+import axios from 'axios'
+
+// This will be our Books Index component (show all books)
+class Posts extends Component {
+  constructor (props) {
+    super(props)
+
+    // setup our initial state
+    this.state = {
+      // we have zero books, until our API request has finished
+      posts: []
+    }
+  }
+
+  // this is called whenever our component is created and inserted
+  // into the DOM (first appears)
+  componentDidMount () {
+    // make a GET request for all of the books
+    console.log(this.state.posts)
+    axios({
+      url: `${apiUrl}/posts`,
+      method: 'GET',
+      headers: {
+        'Authorization': `Token token=${this.props.user.token}`
+      }
+    })
+      .then(res => this.setState({ posts: res.data.posts }))
+      // .then(res => console.log(res))
+      .catch(console.error)
+  }
+
+  render () {
+    const posts = null
+    if (this.state.posts) {
+      const posts = this.state.posts.map(post => (
+        <li key={post._id}>
+          <Link to={`/books/${post._id}`}>
+            {post.title}
+          </Link>
+        </li>
+      ))
+      return posts
+    }
+    return (
+      <div>
+        <h4>Posts</h4>
+        <ul>
+          {posts}
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default Posts
