bb3f5988dfaae9078ac89249c9cb2bcd47fb6a52
Adanetx <Adane1301992@gmail.com>
2020-08-28 21:46:31 -0500

    update commet form

--- src/components/shared/CommentForm.js
+++ src/components/shared/CommentForm.js
@@ -1,39 +1,25 @@
+
 import React from 'react'
 import { Link } from 'react-router-dom'
 import OutlineButton from './OutlineButton.js'
 
-const PostForm = ({ post, handleSubmit, handleChange, cancelPath }) => (
+const CommentForm = ({ comment, handleSubmit, handleChange, cancelPath }) => (
   <div className='long'>
     <form onSubmit={handleSubmit}>
-      <label>Topic:</label><br/>
+      <label>Comment:</label><br/>
       <input
-        placeholder='Enter a topic'
-        value={post.topic}
-        name='topic'
-        onChange={handleChange}
-      /><br/>
-
-      <label>Title:</label><br/>
-      <input
-        placeholder='Enter a title'
-        value={post.title}
-        name='title'
-        onChange={handleChange}
-      /><br/>
-
-      <label>Content:</label><br/>
-      <input
-        placeholder='Content...'
-        value={post.content}
+        placeholder='comment...'
+        value={comment.content}
         name='content'
         onChange={handleChange}
       /><br/>
-      <OutlineButton size="sm" variant="outline-success" type="submit">Submit</OutlineButton>
+      <br/>
+      <OutlineButton variant="success" type="submit">Submit</OutlineButton>
       <Link to={cancelPath}>
-        <OutlineButton size="sm" variant="outline-dark">Cancel</OutlineButton>
+        <OutlineButton variant="dark">Cancel</OutlineButton>
       </Link>
     </form>
   </div>
 )
 
-export default PostForm
+export default CommentForm
