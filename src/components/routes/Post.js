e072cd96b821ae86196aafbc219a588a81385575
Adanetx <Adane1301992@gmail.com>
2020-08-28 21:12:17 -0500

    post update

--- src/components/routes/Post.js
+++ src/components/routes/Post.js
@@ -1,15 +1,7 @@
 import React, { Component } from 'react'
 import { Link, Redirect } from 'react-router-dom'
-<<<<<<< HEAD
-=======
 import CommentIndex from './CommentIndex'
-<<<<<<< HEAD
->>>>>>> development
-
-// import the api's url
-=======
 import OutlineButton from '../shared/OutlineButton.js'
->>>>>>> lastHome
 import apiUrl from '../../apiConfig'
 import axios from 'axios'
 import { withRouter } from 'react-router'
@@ -24,20 +16,11 @@ class Post extends Component {
       post: null,
       // initially this book has not been deleted yet
       deleted: false
-<<<<<<< HEAD
-    }
-  }
-
-  // runs when the component appears (is created and inserted into DOM)
-  componentDidMount () {
-    // make a request to get the book, with the current routes'id
-=======
 
     }
   }
 
   componentDidMount () {
->>>>>>> development
     axios({
       url: `${apiUrl}/posts/${this.props.match.params.id}`,
       method: 'get',
@@ -61,13 +44,7 @@ class Post extends Component {
       .then(() => this.setState({ deleted: true }))
       .catch(console.error)
   }
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-  destroyComment = () => {
-=======
   destroyComment = (commentId) => {
->>>>>>> lastHome
     axios({
       url: `${apiUrl}/posts/${this.props.match.params.id}/comments/${commentId}`,
       method: 'DELETE',
@@ -91,7 +68,6 @@ class Post extends Component {
     const newPath = `/posts/${this.props.match.params.id}/comments/${commentId}/edit`
     this.props.history.push(newPath)
   }
->>>>>>> development
 
   render () {
     const { post, deleted } = this.state
@@ -108,13 +84,7 @@ class Post extends Component {
         state: { message: 'Deleted post successfully' }
       }} />
     }
-<<<<<<< HEAD
-<<<<<<< HEAD
 
-=======
-=======
-
->>>>>>> lastHome
     const commentHtml = (
       <div>
         {this.state.post.comments.map(comment => (
@@ -127,22 +97,6 @@ class Post extends Component {
         ))}
       </div>
     )
-<<<<<<< HEAD
-    console.log(this.state.post.comments)
->>>>>>> development
-    return (
-      <div>
-        <h4>{post.title}</h4>
-        <p>{post.content}</p>
-        <button onClick={this.destroyPost}>Delete Post</button>
-        {/* Add a link to the edit book route when you click the edit button */}
-        <Link to={`/posts/${this.props.match.params.id}/edit`}>
-          <button>Edit</button>
-        </Link>
-<<<<<<< HEAD
-        <Link to='/posts'>Back to all posts</Link>
-=======
-=======
     return (
       <div className='long'>
         <h3>Post:</h3>
@@ -150,17 +104,15 @@ class Post extends Component {
           <h4>{post.title}</h4>
           <p>{post.content}</p>
           <Link to={`/posts/${this.props.match.params.id}/edit`}>
-            <OutlineButton variant="outline-info" size="size">Edit</OutlineButton>
+            <OutlineButton variant="outline-info">Edit</OutlineButton>
           </Link>
           <Link to={`/posts/${this.props.match.params.id}/comments`}>
-            <OutlineButton variant="outline-info" size="sm">Comment</OutlineButton>
+            <OutlineButton variant="outline-info">Comment</OutlineButton>
           </Link>
-          <OutlineButton variant= "outline-danger" size="sm" onClick={this.destroyPost}>Delete Post</OutlineButton>
+          <OutlineButton variant= "outline-danger" onClick={this.destroyPost}>Delete Post</OutlineButton>
         </div>
         <h3>Comments:</h3>
->>>>>>> lastHome
         {commentHtml}
->>>>>>> development
       </div>
     )
   }
