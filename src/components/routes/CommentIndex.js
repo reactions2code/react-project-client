2d5e3366d716a44d54c6b9fe3dd9e8c6705d1c50
loudam88 <loudam88@gmail.com>
2020-08-31 18:24:22 -0500

    Ternary statement
    Hidding buttons from non-owners

--- src/components/routes/CommentIndex.js
+++ src/components/routes/CommentIndex.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import OutlineButton from '../shared/OutlineButton.js'
 
+// const ownerOfComment = (this.user.id === this.comment.owner.id)
+// or
+// const ownerOfComment = (this.user.id && this.comment.owner.id)
+// ownerOfComment
+
 const CommentIndex = ({ content, deleteComment, editComment }) => (
   <div className='comments'>
     {content} <br/>
     <br/>
-    <OutlineButton size="sm" onClick={editComment} variant="outline-info">Edit</OutlineButton>
-    <OutlineButton size="sm" onClick={deleteComment} variant="outline-danger">Delete</OutlineButton>
+    {this.comment
+      ? (<React.Fragment>
+        <OutlineButton size="sm" onClick={editComment} variant="outline-info">Edit</OutlineButton>
+        <OutlineButton size="sm" onClick={deleteComment} variant="outline-danger">Delete</OutlineButton>
+      </React.Fragment>)
+      : ('')}
   </div>
 )
 
