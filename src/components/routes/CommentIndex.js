b39155669de54de4a7729d28c955e1791c56f198
skmidk <s.peraza545@gmail.com>
2020-09-02 09:11:11 -0400

    added comment button show hide feature based on authorization

--- src/components/routes/CommentIndex.js
+++ src/components/routes/CommentIndex.js
@@ -1,13 +1,28 @@
 import React from 'react'
 import OutlineButton from '../shared/OutlineButton.js'
 
-const CommentIndex = ({ content, deleteComment, editComment }) => (
-  <div className='comments'>
-    {content} <br/>
-    <br/>
-    <OutlineButton size="sm" onClick={editComment} variant="outline-info">Edit</OutlineButton>
-    <OutlineButton size="sm" onClick={deleteComment} variant="outline-danger">Delete</OutlineButton>
-  </div>
-)
+const CommentIndex = ({ content, deleteComment, editComment, owner, user }) => {
+  let canEditAndUpdate = ''
+  if (owner === user) {
+    canEditAndUpdate = (
+      <div className='comments'>
+        {content} <br/>
+        <br/>
+        <OutlineButton size="sm" onClick={editComment} variant="outline-info">Edit</OutlineButton>
+        <OutlineButton size="sm" onClick={deleteComment} variant="outline-danger">Delete</OutlineButton>
+      </div>
+    )
+  } else if (owner !== user) {
+    canEditAndUpdate = (
+      <div className='comments'>
+        {content}
+      </div>
+    )
+  }
+
+  return (
+    canEditAndUpdate
+  )
+}
 
 export default CommentIndex
